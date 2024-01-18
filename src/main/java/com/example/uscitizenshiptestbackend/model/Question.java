bc341aa7777f882563843d6403f9fb1f2cfc1b8c
LedyaBakloug <ledya.beshara@yahoo.com>
2024-01-18 10:00:36 -0500

    initial commit

---
+++ src/main/java/com/example/uscitizenshiptestbackend/model/Question.java
@@ -0,0 +1,77 @@
+package com.example.uscitizenshiptestbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Question {
+
+    @Id
+    private Long id;
+    private String question;
+    private String correctAnswer;
+    private String incorrectAnswer1;
+    private String incorrectAnswer2;
+    private String incorrectAnswer3;
+
+    public Question() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getIncorrectAnswer1() {
+        return incorrectAnswer1;
+    }
+
+    public void setIncorrectAnswer1(String incorrectAnswer1) {
+        this.incorrectAnswer1 = incorrectAnswer1;
+    }
+
+    public String getIncorrectAnswer2() {
+        return incorrectAnswer2;
+    }
+
+    public void setIncorrectAnswer2(String incorrectAnswer2) {
+        this.incorrectAnswer2 = incorrectAnswer2;
+    }
+
+    public String getIncorrectAnswer3() {
+        return incorrectAnswer3;
+    }
+
+    public void setIncorrectAnswer3(String incorrectAnswer3) {
+        this.incorrectAnswer3 = incorrectAnswer3;
+    }
+
+    public Question(Long id, String question, String correctAnswer, String incorrectAnswer1, String incorrectAnswer2, String incorrectAnswer3) {
+        this.id = id;
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswer1 = incorrectAnswer1;
+        this.incorrectAnswer2 = incorrectAnswer2;
+        this.incorrectAnswer3 = incorrectAnswer3;
+    }
+}
---
+++ src/test/java/com/example/uscitizenshiptestbackend/UsCitizenshipTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uscitizenshiptestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsCitizenshipTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
